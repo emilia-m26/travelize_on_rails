231e7f0902d70adb034c2d6517ffccd54fe4d5fa
Emilia <emilia.mena26@gmail.com>
2019-11-09 18:38:34 -0500

    editing routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :travelers
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
   resources :destinations
   resources :goals
-
+  resources :travelers
+  
   #custom link of /passport
   get 'passport', :to => 'destinations#index' 
   #custom link or travel inspiration - lists all users
