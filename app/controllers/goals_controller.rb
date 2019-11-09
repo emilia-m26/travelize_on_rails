d77991d95fe0bc85df814c04ecc7279b53e02fff
Emilia <emilia.mena26@gmail.com>
2019-11-09 11:01:15 -0500

    adding new, create, edit, update and destroy

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -9,18 +9,25 @@ class GoalsController < ApplicationController
     end 
 
     def new
+        @goal = Goal.new
     end 
 
     def create
+        @goal = Goal.create(goal_params)
     end
 
     def edit
+        @goal = Goal.find(params[:id])
     end
 
     def update
+        @goal = Goal.find(params[:id])
+        @goal.update(goal_params)
     end 
 
     def destroy
+        @goal = Goal.find(params[:id])
+        @goal.destroy
     end 
 
     private 
