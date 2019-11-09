87798cdcd4ecfa1d3b7cfb70e9d2e77ffe69e8f1
Emilia <emilia.mena26@gmail.com>
2019-11-09 15:07:08 -0500

    created resource travelers

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_09_154601) do
-
-  create_table "destinations", force: :cascade do |t|
-    t.string "location"
-    t.text "description"
-    t.date "date_traveled"
-    t.boolean "completed"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "goals", force: :cascade do |t|
-    t.string "name"
-    t.integer "user_id"
-    t.integer "destination_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
