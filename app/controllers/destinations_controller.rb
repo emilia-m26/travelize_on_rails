b9532e21624c522974579212c12f23444c778276
Emilia <emilia.mena26@gmail.com>
2019-11-09 15:44:42 -0500

    comments to remind of future functionality

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,5 +1,8 @@
 class DestinationsController < ApplicationController
 
     def index
+        #using for passport page
+        #will list a traveler's completed destinations
     end 
+
 end
--- app/controllers/travelers_controller.rb
+++ app/controllers/travelers_controller.rb
@@ -1,6 +1,8 @@
 class TravelersController < ApplicationController
 
     def index
+        #using for travel inspiration page
+        #where all travelers will be listed
     end 
 
     
