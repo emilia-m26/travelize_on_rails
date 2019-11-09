390a1504998b053ae497304e4474ecc40f439402
Emilia <emilia.mena26@gmail.com>
2019-11-09 10:58:51 -0500

    adding goals index,show, and strong params

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -1,9 +1,11 @@
 class GoalsController < ApplicationController
 
     def index
+        @goals = Goal.all
     end
 
     def show
+        @goal = Goal.find(params[:id])
     end 
 
     def new
@@ -19,6 +21,12 @@ class GoalsController < ApplicationController
     end 
 
     def destroy
+    end 
+
+    private 
+
+    def goal_params #defining strong params
+        params.require(:goal).permit(:name, :user_id, :destination_id)
     end 
     
 end
