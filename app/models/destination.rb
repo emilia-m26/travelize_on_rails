dc6ac53da019bf63c8bdea0fac8406b67a31167d
Emilia <emilia.mena26@gmail.com>
2019-11-10 17:11:42 -0500

    changed show page to show destination locations, new form muted future functionality, added validations to goal and destination models

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -2,4 +2,6 @@ class Destination < ApplicationRecord
      #belongs_to :traveler
      belongs_to :goal
 
+     validates_presence_of :location
+
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,7 +1,7 @@
 class Goal < ApplicationRecord
     has_many :destinations
     has_many :travelers, through: :destinations
-    accepts_nested_attributes_for :destinations
+    accepts_nested_attributes_for :destinations, reject_if: :all_blank #all_blank works if no checkbox
     #field_for checks for the above macro to see if it is present
     #before creating more than one field for destinations
     
