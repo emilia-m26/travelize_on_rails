84f31079cbf342c3801dd4b6f3dec974bfe6e0a7
Emilia <emilia.mena26@gmail.com>
2019-11-10 15:34:48 -0500

    unmuting validations

--- app/models/traveler.rb
+++ app/models/traveler.rb
@@ -3,7 +3,7 @@ class Traveler < ApplicationRecord
     has_many :goals, through: :destinations
 
     has_secure_password
-    # validates_presence_of :name, :password, :password_confirmation
-    # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
-    # validates :email, uniqueness: true
+    validates_presence_of :name, :password, :password_confirmation
+    validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
+    validates :email, uniqueness: true
 end
