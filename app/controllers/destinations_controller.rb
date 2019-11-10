4f2dc9fc14cb5195912820aabb55f6056afd0f85
Emilia <emilia.mena26@gmail.com>
2019-11-10 12:43:19 -0500

    muted relationships will building routes for destinations controller

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -5,4 +5,22 @@ class DestinationsController < ApplicationController
         #will list a traveler's completed destinations
     end 
 
+    def show
+    end 
+
+    def new
+    end 
+    
+    def create
+    end 
+
+    def edit
+    end
+
+    def update
+    end 
+
+    def destroy
+    end
+
 end
--- app/models/goal.rb
+++ app/models/goal.rb
@@ -1,6 +1,6 @@
 class Goal < ApplicationRecord
-    belongs_to :traveler
-    belongs_to :destination
+    # belongs_to :traveler
+    # belongs_to :destination
     
     validates :name, presence: :true
     #validations runs on create, update, and save
