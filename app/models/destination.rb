791a5447ea751d8fc399cd14ea194edb3ed39ba0
Emilia <emilia.mena26@gmail.com>
2019-11-11 07:38:00 -0500

    setting up helper method to display date traveler completed their travel, for passport page displays

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,4 +4,8 @@ class Destination < ApplicationRecord
 
      validates_presence_of :location
 
+     def travel_date #helper method to use later for passport page, to display date traveled info
+         self.date_traveled.strftime("%b %Y") 
+     end  
+
 end
