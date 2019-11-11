e37846ed49d3babcac2ce8122767089ed5645bf4
Emilia <emilia.mena26@gmail.com>
2019-11-11 07:21:37 -0500

    made change to goals controller - strong params, added partial for goals views of edit and new

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -10,7 +10,7 @@ class GoalsController < ApplicationController
 
     def new
        @goal = Goal.new
-       10.times{@goal.destinations.build} #getting destination_id and traveler_id automatically
+       5.times{@goal.destinations.build} #getting destination_id and traveler_id automatically
     end 
 
     def create
@@ -45,7 +45,7 @@ class GoalsController < ApplicationController
     private 
 
     def goal_params #defining strong params
-        params.require(:goal).permit(:name, destinations_attributes:[:location, :description, :date_traveled, :completed, :traveler_id, :goal_id])
+        params.require(:goal).permit(:name, destinations_attributes:[:id, :location, :description, :date_traveled, :completed, :traveler_id, :goal_id])
     end 
 
     def find_goal
