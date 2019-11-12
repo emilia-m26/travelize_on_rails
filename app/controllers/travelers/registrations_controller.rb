468832aec627e932d2f3e33f166776f0c55511fc
Emilia <emilia.mena26@gmail.com>
2019-11-12 16:45:37 -0500

    changes to nav bar, adjust devise default settings to match my needs

--- app/controllers/travelers/registrations_controller.rb
+++ app/controllers/travelers/registrations_controller.rb
@@ -38,16 +38,16 @@ class Travelers::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  #protected
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
   # end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
-  #   devise_parameter_sanitizer.permit(:account_update, keys: [:attribute])
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name, :username])
   # end
 
   # The path used after sign up.
