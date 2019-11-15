467930b260572ed96fc024a45d039364f785b7ae
Emilia <emilia.mena26@gmail.com>
2019-11-15 17:09:13 -0500

    limiting access to only logged in travelers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+     #will not allow for someone not logged in to see any information
+    before_action :authenticate_traveler!, :except => [:inspiration]
+   
+
     def welcome
     end
 
