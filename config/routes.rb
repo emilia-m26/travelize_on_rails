4c8dbf2a62f574ed76a64e7904c3dc920481bfe6
Emilia <emilia.mena26@gmail.com>
2019-11-21 06:34:01 -0500

    cleaning up routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :travelers, :controllers => { registrations: 'registrations', omniauth_callbacks: 'callbacks' }
-    # devise_scope :traveler do
-    #   get 'login', to: 'devise/sessions#new'
-    # end 
-    # devise_scope :traveler do
-    #   get 'signup', to: 'devise/registrations#new'
-    # end 
-
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_for :travelers, :controllers => { registrations: 'registrations', omniauth_callbacks: 'callbacks' }
 
   root to: "application#welcome"
   #resources :destinations
   resources :goals, except: [:show]
 
+  #custom travel-inspiration page
   get '/travel-inspiration', to:"application#inspiration"
-
+  #custom passport page
   get '/passport', to:"application#passport"
 
-  #get '/destinations', to: "destinations#index", as:"destinations"
-  #nested
+  #nested index
   get '/goals/:id/destinations', to:"destinations#index", as:"goal_destinations"
   
-  #nested
+  #nested new
   get '/goals/:id/destinations/new', to:"destinations#new", as:"new_goal_destination"
   post '/destinations', to:"destinations#create" #without this,form goes nowhere
 
   #nested show route to dispaly specific destination details
   get 'goals/:goal_id/destinations/:id', to:"destinations#show", as:"goal_destination"
-
-  # resources :goals do
-  #   resources :destinations
-  # end 
 
 end
 
