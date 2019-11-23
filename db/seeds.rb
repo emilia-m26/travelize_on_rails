4e7e32bd47174a53adbb5c92b2d0d2e1d37b7535
Emilia <emilia.mena26@gmail.com>
2019-11-22 21:57:47 -0500

    seed file run

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,38 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Traveler.create(name: "Emilia", username:"Emi", email: "emi@example.com", password: "password")
+Traveler.create(name: "Alex", username:"Alex 5000", email: "alex@example.com", password: "password")
+Traveler.create(name: "Filippe", username:"Flamengo Surfer", email: "surfer@example.com", password: "password")
+
+Goal.create(name: "Travel Brazil") #goal 1
+Goal.create(name: "Travel United States") #goal 2
+Goal.create(name: "Travel South America") #goal 3
+Goal.create(name: "Travel to Best Surfer Spots") #goal 4
+Goal.create(name: "World Travel") #goal 5
+
+Destination.create(location:"Rio de Janeiro", description:"A Cidade Maravilhosa!", date_traveled:"10/02/2017", completed:true, traveler_id:1, goal_id:1)
+Destination.create(location:"Sao Paulo", completed:false, traveler_id:1, goal_id:1)
+Destination.create(location:"Curitiba", completed:false, traveler_id:1, goal_id:1)
+Destination.create(location:"Lencois Maranheses", description:"The 4-wheeler tour was amazing!", date_traveled:"03/13/2018", completed:true, traveler_id:1, goal_id:1)
+Destination.create(location:"Amazonas", completed:false, traveler_id:1, goal_id:1)
+Destination.create(location:"Minas Gerais", description:"Inhotim is super cool!", date_traveled:"02/20/2018", completed:true, traveler_id:1, goal_id:1)
+Destination.create(location:"Greece", completed:false, traveler_id:1, goal_id:5)
+Destination.create(location:"Brazil", completed:false, traveler_id:1, goal_id:5)
+Destination.create(location:"India", completed:false, traveler_id:1, goal_id:5)
+Destination.create(location:"Japan", completed:false, traveler_id:1, goal_id:5)
+Destination.create(location:"Australia", completed:false, traveler_id:1, goal_id:5)
+
+
+Destination.create(location:"California", completed:false, traveler_id:2, goal_id:2)
+Destination.create(location:"Texas", description: "Texas is my home!", completed:true, traveler_id:2, goal_id:2)
+Destination.create(location:"Miami", completed:true, traveler_id:2, goal_id:2)
+Destination.create(location:"Argentina", completed:false, traveler_id:2, goal_id:3)
+Destination.create(location:"Peru", completed:false, traveler_id:2, goal_id:3)
+Destination.create(location:"Chile", completed:false, traveler_id:2, goal_id:3)
+Destination.create(location:"Costa Rica", completed:false, traveler_id:2, goal_id:3)
+
+Destination.create(location:"Hawaii", completed:false, traveler_id:3, goal_id:4)
+Destination.create(location:"California", completed:false, traveler_id:3, goal_id:4)
+Destination.create(location:"Tahiti", completed:false, traveler_id:3, goal_id:4)
+Destination.create(location:"France", completed:true, traveler_id:3, goal_id:4)
+Destination.create(location:"Germany", completed:false, traveler_id:3, goal_id:4)
+Destination.create(location:"Portugal", completed:false, traveler_id:3, goal_id:4)
+Destination.create(location:"El Salvador", completed:false, traveler_id:3, goal_id:4)
