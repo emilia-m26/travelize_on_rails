a17678123480913143b3bbc29a8448ff59c34fb1
Emilia <emilia.mena26@gmail.com>
2019-11-23 17:34:34 -0500

    deleting unnecessary comments

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -4,20 +4,14 @@ class Destination < ApplicationRecord
 
      validates_presence_of :location
 
-     #helper method to use for travel inspiration page, when displaying travel experience for a specific user
-
      def self.travel_experience(traveler)
         #travel experience or specific traveler
          where(completed: true, traveler_id: traveler.id).count 
-         #in console used Destination.where(completed:true, traveler_id:2).count
-         #=> 1
      end  
 
-     def self.passport(traveler)
-          where(completed: true, traveler_id: traveler.id)
-          #Destination.where(completed:true, traveler_id:1)
-     end
-
-       
+     # def self.passport(traveler)
+     #      where(completed: true, traveler_id: traveler.id)
+     #      #Destination.where(completed:true, traveler_id:1)
+     # end
 
 end
